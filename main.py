f33637171595182a5184d282f27f03b7ca9e9771
Tim Baker <Tizamarou@gmail.com>
2018-04-02 13:53:45 -0500

    Finally Finished thisgit add . Didnt need arguments in function encrypt

--- main.py
+++ main.py
@@ -1,52 +1,65 @@
-from flask import Flask
+from flask import Flask, request
+
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-page_header = """
+form = """
 <! DOCTYPE html>
 
 <html>
     <head>
         <style>
-        """
-
-form = """
-{
-background-color: #eee;
-padding: 20px;
-margin: 0 auto;
-width: 540px;
-font: 16px sans-serif;
-border-radius: 10px;
-}
-"""
-textarea = """{
-margin: 10px 0;
-width: 540px;
-height: 120px;
-}
-"""
-rest_code = """
-</style>
-</head>
-<body>
-<form method="post">
-        <label>
-            Rotated by <input type="text" value= 0 name="rot"/>
-        </label>
-        <input type="submit" value="Submit"/>
-        <input type="textarea" name = "text"/>
-    </form>
-</body>
+            form {{
+        background-color: #eee;
+        padding: 20px;
+        margin: 0 auto;
+        width: 540px;
+        font: 16px sans-serif;
+        border-radius: 10px;
+        }}
+        textarea {{
+        margin: 10px 0;
+        width: 540px;
+        height: 120px;
+        }}
+        </style>
+    </head>
+    <body>
+        <form method="post">
+            <div>
+                <label>Rotate by:</label>
+                <input type="text" name="rot" value= 0 />
+            </div>
+                <textarea type ="text" name="text">{0}</textarea>
+            <br>
+            <input type="submit" value="Submit"/>
+        </form>
+    </body>
 </html>
 """
 
 
+
 @app.route("/")
 def index():
-    content = page_header + form + textarea + rest_code
+    
+    blank = ''
+    return form.format(blank)
 
-    return content
+
+@app.route("/", methods=['POST'])
+
+def encrypt():
+    rot = int(request.form['rot'])
+    text = request.form['text']
+    new_text = rotate_string(text, rot)
+    new_text = '<h1>' + form.format(new_text) + '</h1>'
+    
+    
+    return new_text
+
+
 
 app.run()
